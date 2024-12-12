fa70d64e8048b3d9d2b4a56e21f50d98951503e8
Dlamar <dlamardima@gmail.com>
2024-12-12 14:45:49 +0300

    add test

---
+++ tests/deposit.test.ts
@@ -0,0 +1,40 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program, BN } from "@coral-xyz/anchor";
+import { TestChlen } from "../target/types/test_chlen";
+import { deposit, setupDepositTest } from "./utils";
+
+describe("deposit test", () => {
+  anchor.setProvider(anchor.AnchorProvider.env());
+  const owner = anchor.Wallet.local().payer;
+
+  const program = anchor.workspace.TestChlen as Program<TestChlen>;
+
+  const confirmOptions = {
+    skipPreflight: true,
+  };
+
+  it("deposit test", async () => {
+    const cpSwapPoolState = await setupDepositTest(
+      program,
+      anchor.getProvider().connection,
+      owner,
+      { transferFeeBasisPoints: 0, MaxFee: 0 }
+    );
+
+    const liquidity = new BN(10000000000);
+    const depositTx = await deposit(
+      program,
+      owner,
+      cpSwapPoolState.ammConfig,
+      cpSwapPoolState.token0Mint,
+      cpSwapPoolState.token0Program,
+      cpSwapPoolState.token1Mint,
+      cpSwapPoolState.token1Program,
+      liquidity,
+      new BN(10000000000),
+      new BN(20000000000),
+      confirmOptions
+    );
+    console.log("depositTx:", depositTx);
+  });
+});
---
+++ tests/swap.test.ts
@@ -0,0 +1,72 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program, BN } from "@coral-xyz/anchor";
+import { TestChlen } from "../target/types/test_chlen";
+import { setupSwapTest, swap_base_input, swap_base_output } from "./utils";
+import { getAccount, getAssociatedTokenAddressSync } from "@solana/spl-token";
+import { configAddress } from "./config";
+
+describe("swap test", () => {
+  anchor.setProvider(anchor.AnchorProvider.env());
+  const owner = anchor.Wallet.local().payer;
+
+  const program = anchor.workspace.TestChlen as Program<TestChlen>;
+
+  const confirmOptions = {
+    skipPreflight: true,
+  };
+
+  it("swap base input", async () => {
+    const cpSwapPoolState = await setupSwapTest(
+      program,
+      anchor.getProvider().connection,
+      owner,
+      { transferFeeBasisPoints: 0, MaxFee: 0 }
+    );
+    const inputToken = cpSwapPoolState.token0Mint;
+    const inputTokenProgram = cpSwapPoolState.token0Program;
+    await sleep(1000);
+    let amount_in = new BN(100000000);
+    const baseInTx = await swap_base_input(
+      program,
+      owner,
+      configAddress,
+      inputToken,
+      inputTokenProgram,
+      cpSwapPoolState.token1Mint,
+      cpSwapPoolState.token1Program,
+      amount_in,
+      new BN(0)
+    );
+    console.log("baseInputTx:", baseInTx);
+  });
+
+  it("swap base output ", async () => {
+    const cpSwapPoolState = await setupSwapTest(
+      program,
+      anchor.getProvider().connection,
+      owner,
+      { transferFeeBasisPoints: 0, MaxFee: 0 }
+    );
+    const inputToken = cpSwapPoolState.token0Mint;
+    const inputTokenProgram = cpSwapPoolState.token0Program;
+    await sleep(1000);
+    let amount_out = new BN(100000000);
+    const baseOutTx = await swap_base_output(
+      program,
+      owner,
+      configAddress,
+      inputToken,
+      inputTokenProgram,
+      cpSwapPoolState.token1Mint,
+      cpSwapPoolState.token1Program,
+      amount_out,
+      new BN(10000000000000),
+      confirmOptions
+    );
+    console.log("baseOutputTx:", baseOutTx);
+  });
+});
+
+function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
