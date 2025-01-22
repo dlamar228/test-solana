f44dbb3dcff2cb2d6bfd078c266ec2f9c83627b0
Dlamar <dlamardima@gmail.com>
2025-01-22 17:51:53 +0300

    add launcher team token withdraw

--- programs/launcher/src/instructions/authority_manager.rs
+++ programs/launcher/src/instructions/authority_manager.rs
@@ -1,7 +1,9 @@
-use crate::errors::ErrorCode;
-use crate::states::*;
-
 use super::*;
+
+use crate::states::*;
+use crate::{errors::ErrorCode, utils::TokenUtils};
+
+use anchor_spl::token_interface::{Mint, TokenAccount, TokenInterface};
 
 pub fn initialize_authority_manager(
     ctx: Context<InitializeAuthorityManager>,
@@ -76,26 +78,67 @@ pub struct UpdateAuthorityManager<'info> {
     pub authority_manager: Account<'info, AuthorityManager>,
 }
 
+pub fn withdraw_team_tokens(ctx: Context<WithdrawTeamTokens>) -> Result<()> {
+    let seeds = [
+        LAUNCHER_AUTHORITY_SEED.as_bytes(),
+        &[ctx.accounts.authority_manager.authority_bump],
+    ];
+    let signer_seeds = &[seeds.as_slice()];
+
+    let token_utils = TokenUtils {
+        mint: ctx.accounts.mint.to_account_info(),
+        decimals: ctx.accounts.mint.decimals,
+        token_program: ctx.accounts.token_program.to_account_info(),
+    };
+
+    token_utils.transfer_signer(
+        ctx.accounts.authority.to_account_info(),
+        ctx.accounts.team_vault.to_account_info(),
+        ctx.accounts.recipient.to_account_info(),
+        ctx.accounts.team_vault.amount,
+        signer_seeds,
+    )?;
+
+    Ok(())
+}
+
 #[derive(Accounts)]
-pub struct TestCpi<'info> {
-    #[account(mut, address = authority_manager.admin @ ErrorCode::InvalidAdmin)]
+pub struct WithdrawTeamTokens<'info> {
+    #[account(address = authority_manager.admin @ ErrorCode::InvalidAdmin)]
     pub payer: Signer<'info>,
-    #[account(
-        seeds = [LAUNCHER_AUTHORITY_MANAGER_SEED.as_bytes()],
-        bump = authority_manager.bump
-    )]
-    pub authority_manager: Account<'info, AuthorityManager>,
-    /// CHECK:
     #[account(
         mut,
         seeds = [
-            LAUNCHER_AUTHORITY_SEED.as_bytes(),
+            LAUNCHER_AUTHORITY_SEED.as_bytes()
         ],
-        bump = authority_manager.authority_bump,
+        bump = authority_manager.authority_bump
     )]
     pub authority: AccountInfo<'info>,
-    /// CHECK: dex vault authority
-    pub dex_program: UncheckedAccount<'info>,
-    /// CHECK: dex vault authority
-    pub dex_authority_manager: UncheckedAccount<'info>,
+    #[account(
+        mut,
+        seeds = [LAUNCHER_AUTHORITY_MANAGER_SEED.as_bytes()],
+        bump = authority_manager.bump
+    )]
+    pub authority_manager: Box<Account<'info, AuthorityManager>>,
+    #[account(
+        mut,
+        token::mint = mint,
+        token::authority = authority,
+        token::token_program = token_program,
+        seeds = [LAUNCHER_TEAM_VAULT_SEED.as_bytes(), mint.key().as_ref()],
+        bump,
+
+    )]
+    pub team_vault: Box<InterfaceAccount<'info, TokenAccount>>,
+    #[account(
+        mut,
+        token::mint = mint,
+        token::token_program = token_program,
+    )]
+    pub recipient: Box<InterfaceAccount<'info, TokenAccount>>,
+    #[account(
+        mint::token_program = token_program,
+    )]
+    pub mint: Box<InterfaceAccount<'info, Mint>>,
+    pub token_program: Interface<'info, TokenInterface>,
 }
